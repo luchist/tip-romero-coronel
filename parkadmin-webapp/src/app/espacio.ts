4c7917fef3d348bf1e747696fe4ce7de1b37d93f
luchist <lucho_9292@live.com>
2019-04-17 18:53:22 -0300

    model: espacio, nivel

---
+++ parkadmin-webapp/src/app/espacio.ts
@@ -0,0 +1,30 @@
+export class Espacio {
+    x: number;
+    y: number;
+    height: number;
+    width: number;
+
+    constructor(x:number, y:number, height:number, width: number) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+    }
+
+}
+
+class Auto extends Espacio {
+
+    constructor(x:number, y:number) {
+        super(x,y,4.5,2.5);
+    }
+
+}
+
+class Moto extends Espacio {
+
+    constructor(x:number, y:number) {
+        super(x,y,2.5,1.2);
+    }
+
+}
